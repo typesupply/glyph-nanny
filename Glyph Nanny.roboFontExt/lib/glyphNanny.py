0a6b763e1185a93e444442a3e33706cb7bb4128e
Tal Leming <tal@typesupply.com>
2015-08-26 13:09:29 -0400

    Add a stem consistency test.

--- Glyph Nanny.roboFontExt/lib/glyphNanny.py
+++ Glyph Nanny.roboFontExt/lib/glyphNanny.py
@@ -2,6 +2,7 @@ import os
 import re
 import math
 from fontTools.misc import bezierTools as ftBezierTools
+from fontTools.misc import arrayTools as ftArrayTools
 from fontTools.agl import AGL2UV
 from fontTools.pens.cocoaPen import CocoaPen
 from robofab.pens.digestPen import DigestPointPen
@@ -297,6 +298,7 @@ unicodeValue
 componentMetrics
 ligatureMetrics
 metricsSymmetry
+stemWidths
 strayPoints
 smallContours
 openContours
@@ -318,6 +320,7 @@ unicodeValue
 componentMetrics
 ligatureMetrics
 metricsSymmetry
+stemWidths
 smallContours
 pointsNearVerticalMetrics
 complexCurves
@@ -516,6 +519,221 @@ registerTest(
     testFunction=testUnicodeValue,
     drawingFunction=None
 )
+
+
+# Stem Consistency
+
+def testStemWidths(glyph):
+    """
+    Test for stem width consistency.
+    """
+    hProblems = vProblems = None
+    font = glyph.getParent()
+    tolerance = 5
+    # horizontal
+    hStems = [_StemWrapper(v, tolerance) for v in font.info.postscriptStemSnapH]
+    if hStems:
+        hProblems = _findStems(glyph, hStems, "h")
+    # vertical
+    vStems = [_StemWrapper(v, tolerance) for v in font.info.postscriptStemSnapV]
+    if vStems:
+        vProblems = _findStems(glyph, vStems, "v")
+    # report
+    stemProblems = dict(
+        h=hProblems,
+        v=vProblems
+    )
+    return stemProblems
+
+def drawStemWidths(data, scale, glyph):
+    hProblems = data["h"]
+    vProblems = data["v"]
+    if not hProblems and not vProblems:
+        return
+    font = glyph.getParent()
+    b = font.info.unitsPerEm * 0.25
+    color = textColor = colorReview()
+    color = modifyColorAlpha(color, 0.1)
+    # horizontal
+    x = -b
+    w = glyph.width + (b * 2)
+    for y1, y2 in hProblems:
+        h = y2 - y1
+        color.set()
+        rect = ((x, y1), (w, h))
+        NSRectFillUsingOperation(rect, NSCompositeSourceOver)
+        pt = (20, y1 + (h / 2) - 5)
+        drawString(pt, "Check Stem", 10, scale, textColor, alignment="left")
+    # horizontal
+    y = font.info.descender - b
+    h = max((font.info.ascender, font.info.capHeight)) - y + (b * 2)
+    for x1, x2 in vProblems:
+        w = x2 - x1
+        color.set()
+        rect = ((x1, y), (w, h))
+        NSRectFillUsingOperation(rect, NSCompositeSourceOver)
+        pt = (x1 + (w / 2), -20)
+        drawString(pt, "Check Stem", 10, scale, textColor, alignment="center")
+
+registerTest(
+    identifier="stemWidths",
+    level="glyph",
+    title="Stem Widths",
+    description="One or more stems do not match the registered values.",
+    testFunction=testStemWidths,
+    drawingFunction=drawStemWidths
+)
+
+def _findStems(glyph, targetStems, stemDirection):
+    stems = set()
+    # h/v abstraction
+    if stemDirection == "h":
+        primaryCoordinate = 1
+        secondaryCoordinate = 0
+        desiredClockwiseAngle = 0
+        desiredCounterAngle = 180
+    else:
+        primaryCoordinate = 0
+        secondaryCoordinate = 1
+        desiredClockwiseAngle = -90
+        desiredCounterAngle = 90
+    # structure the contour and line data for efficient processing
+    contours = {
+        True : [],
+        False : []
+    }
+    for contour in glyph:
+        contourDirection = contour.clockwise
+        if hasattr(contour, "bounds"):
+            bounds = contour.bounds
+        else:
+            bounds = contour.box
+        lines = {}
+        # line to
+        previous = _unwrapPoint(contour[-1].onCurve)
+        for segment in contour:
+            point = _unwrapPoint(segment.onCurve)
+            if segment.type == "line":
+                # only process completely horizontal/vertical lines
+                # that have a length grater than 0
+                if (previous[primaryCoordinate] == point[primaryCoordinate]) and (previous[secondaryCoordinate] != point[secondaryCoordinate]):
+                    angle = _calcAngle(previous, point)
+                    p = point[primaryCoordinate]
+                    s1 = previous[secondaryCoordinate]
+                    s2 = point[secondaryCoordinate]
+                    s1, s2 = sorted((s1, s2))
+                    if angle not in lines:
+                        lines[angle] = {}
+                    if p not in lines[angle]:
+                        lines[angle][p] = []
+                    lines[angle][p].append((s1, s2))
+            previous = point
+        # imply stems from curves by using BCP handles
+        previous = contour[-1]
+        for segment in contour:
+            if segment.type == "curve" and previous.type == "curve":
+                bcp1 = _unwrapPoint(previous[1])
+                bcp2 = _unwrapPoint(segment[-1])
+                if bcp1[primaryCoordinate] == bcp1[primaryCoordinate]:
+                    angle = _calcAngle(bcp1, bcp2)
+                    p = bcp1[primaryCoordinate]
+                    s1 = bcp1[secondaryCoordinate]
+                    s2 = bcp2[secondaryCoordinate]
+                    s1, s2 = sorted((s1, s2))
+                    if angle not in lines:
+                        lines[angle] = {}
+                    if p not in lines[angle]:
+                        lines[angle][p] = []
+                    lines[angle][p].append((s1, s2))
+            previous = segment
+        contours[contourDirection].append((bounds, lines))
+    # single contours
+    for clockwise, directionContours in contours.items():
+        for contour in directionContours:
+            bounds, data = contour
+            for angle1, lineData1 in data.items():
+                for angle2, lineData2 in data.items():
+                    if angle1 == angle2:
+                        continue
+                    if clockwise and angle1 == desiredClockwiseAngle:
+                        continue
+                    if not clockwise and angle1 == desiredCounterAngle:
+                        continue
+                    for p1, lines1 in lineData1.items():
+                        for p2, lines2 in lineData2.items():
+                            if p2 <= p1:
+                                continue
+                            for s1a, s1b in lines1:
+                                for s2a, s2b in lines2:
+                                    overlap = _linesOverlap(s1a, s1b, s2a, s2b)
+                                    if not overlap:
+                                        continue
+                                    w = p2 - p1
+                                    hits = []
+                                    for stem in targetStems:
+                                        if w == stem:
+                                            d = stem.diff(w)
+                                            if d:
+                                                hits.append((d, stem.value))
+                                    if hits:
+                                        w = min(hits)[1]
+                                        stems.add((p1, p1 + w))
+    # double contours to test
+    for clockwiseContour in contours[True]:
+        clockwiseBounds = clockwiseContour[0]
+        for counterContour in contours[False]:
+            counterBounds = counterContour[0]
+            overlap = ftArrayTools.sectRect(clockwiseBounds, counterBounds)[0]
+            if not overlap:
+                continue
+            clockwiseData = clockwiseContour[1]
+            counterData = counterContour[1]
+            for clockwiseAngle, clockwiseLineData in clockwiseContour[1].items():
+                for counterAngle, counterLineData in counterContour[1].items():
+                    if clockwiseAngle == counterAngle:
+                        continue
+                    for clockwiseP, clockwiseLines in clockwiseLineData.items():
+                        for counterP, counterLines in counterLineData.items():
+                            for clockwiseSA, clockwiseSB in clockwiseLines:
+                                for counterSA, counterSB in counterLines:
+                                    overlap = _linesOverlap(clockwiseSA, clockwiseSB, counterSA, counterSB)
+                                    if not overlap:
+                                        continue
+                                    w = abs(counterP - clockwiseP)
+                                    hits = []
+                                    for stem in targetStems:
+                                        if w == stem:
+                                            d = stem.diff(w)
+                                            if d:
+                                                hits.append((d, stem.value))
+                                    if hits:
+                                        p = min((clockwiseP, counterP))
+                                        w = min(hits)[1]
+                                        stems.add((p, p + w))
+    # done
+    return stems
+
+
+class _StemWrapper(object):
+
+    def __init__(self, value, threshold):
+        self.value = value
+        self.threshold = threshold
+    
+    def __cmp__(self, other):
+        d = abs(self.value - other)
+        if d <= self.threshold:
+            return 0
+        return cmp(self.value, other)
+
+    def diff(self, other):
+        return abs(self.value - other)
+
+
+def _linesOverlap(a1, a2, b1, b2):
+    if a1 > b2 or a2 < b1:
+        return False
+    return True
 
 
 # -------------------
