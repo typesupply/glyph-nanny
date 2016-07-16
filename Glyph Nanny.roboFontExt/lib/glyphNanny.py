5636bde15434b95031c654cfaf96493235a70e88
Tal Leming <tal@typesupply.com>
2016-07-16 11:03:17 -0400

    Changes from @typemytype.

--- Glyph Nanny.roboFontExt/lib/glyphNanny.py
+++ Glyph Nanny.roboFontExt/lib/glyphNanny.py
@@ -16,8 +16,10 @@ from mojo.roboFont import CurrentFont, AllFonts
 from mojo.roboFont import version as roboFontVersion
 from mojo.UI import UpdateCurrentGlyphView, HTMLView
 from mojo.events import addObserver, removeObserver
-from mojo.extensions import getExtensionDefault, setExtensionDefault, getExtensionDefaultColor, setExtensionDefaultColor
+from mojo.extensions import getExtensionDefault, setExtensionDefault, setExtensionDefaultColor
 from lib.tools import bezierTools as rfBezierTools
+
+from defaultManager import defaults, defaultKeyObserverVisibility, defaultKeyTitleVisibility, defaultKeyTestStates, defaultKeyColorInform, defaultKeyColorReview, defaultKeyColorRemove, defaultKeyColorInsert
 
 DEBUG = False
 
@@ -30,14 +32,6 @@ highlightLineWidth = 4
 textSize = 10
 textVerticalOffset = 10
 
-defaultKeyStub = "com.typesupply.GlyphNanny."
-defaultKeyObserverVisibility = defaultKeyStub + "displayReportInGlyphView"
-defaultKeyTitleVisibility = defaultKeyStub + "displayReportTitles"
-defaultKeyTestStates = defaultKeyStub + "testStates"
-defaultKeyColorInform = defaultKeyStub + "colorInform"
-defaultKeyColorReview = defaultKeyStub + "colorReview"
-defaultKeyColorRemove = defaultKeyStub + "colorRemove"
-defaultKeyColorInsert = defaultKeyStub + "colorInsert"
 
 def registerGlyphNannyDefaults():
     storage = {
@@ -64,58 +58,7 @@ def registerGlyphNannyDefaults():
     defaults.reload()
 
 
-class DefaultsManager(object):
 
-    def __init__(self):
-        self._values = {}
-
-    def reload(self):
-        self._values = {}
-
-    # values
-
-    def getValue(self, key):
-        if key not in self._values:
-            self._values[key] = getExtensionDefault(key)
-        return self._values[key]
-
-    def _get_showTitles(self):
-        return self.getValue(defaultKeyTitleVisibility)
-
-    showTitles = property(_get_showTitles)
-
-    # Colors
-
-    def _getColor(self, key, fallback):
-        if key not in self._values:
-            color = getExtensionDefaultColor(key)
-            if color is None:
-                color = NSColor.colorWithCalibratedRed_green_blue_alpha_(*fallback)
-            self._values[key] = color
-        return self._values[key]
-
-    def _get_colorInform(self):
-        return self._getColor(defaultKeyColorInform, (0, 0, 0.7, 0.3))
-
-    colorInform = property(_get_colorInform)
-
-    def _get_colorInsert(self):
-        return self._getColor(defaultKeyColorInsert, (0, 1, 0, 0.75))
-
-    colorInsert = property(_get_colorInsert)
-
-    def _get_colorRemove(self):
-        return self._getColor(defaultKeyColorRemove, (1, 0, 0, 0.5))
-
-    colorRemove = property(_get_colorRemove)
-
-    def _get_colorReview(self):
-        return self._getColor(defaultKeyColorReview, (1, 0.7, 0, 0.7))
-
-    colorReview = property(_get_colorReview)
-
-
-defaults = DefaultsManager()
 
 # ----------------
 # Drawing Observer
@@ -1745,7 +1688,7 @@ def _makeZonePairs(blues):
     if not len(blues) % 2:
         while blues:
             bottom = blues.pop(0)
-            top = blues.pop(0) 
+            top = blues.pop(0)
             pairs.append((bottom, top))
     return pairs
 
